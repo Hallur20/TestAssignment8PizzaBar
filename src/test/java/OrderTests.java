afacdc83a3f735bcc0b21b7b363310fbbad637d3
Hallur20 <hvn15@hotmail.com>
2019-03-27 23:23:28 +0100

    removed unnessasery code

--- src/test/java/OrderTests.java
+++ src/test/java/OrderTests.java
@@ -25,10 +25,8 @@ public class OrderTests {
     WebDriver driver;
     Order pepperoniOrder;
 
-    //@Mock
     Customer c1, c2;
 
-    //@Mock
     Employee e;
 
     @Mock
@@ -41,7 +39,6 @@ public class OrderTests {
         driver.get("localhost:8080/MarioPizzaWeb/");
         orders = new ArrayList<>();
     }
-
     @When("user picks the \"pepperoni\" pizza")
     public void pickPizza() {
         driver.findElement(By.id("pepperoni")).click();
@@ -49,14 +46,12 @@ public class OrderTests {
         c2 = mock(Customer.class);
         when(c2.makeOrder()).thenReturn(pepperoniOrder);
     }
-
     @Then("user clicks \"order\"")
     public void makeOrder() {
         driver.findElement(By.id("orderBtn")).click();
         orders.add(c2.makeOrder());
         System.out.println("orders size: " + orders.size());
     }
-
     @Then("employee deletes order from user")
     public void deleteOrder() {
         e = mock(Employee.class);
@@ -72,36 +67,5 @@ public class OrderTests {
         e.deleteOrder(c2);
         Assert.assertThat(orders.size(), CoreMatchers.equalTo(0));
         
-    }
-
-    @Test
-    public void bla() {
-        ArrayList<Order> orders = new ArrayList<>(); //list for pizza orders
-        Order margeritaOrder = new Order(1, "Margerita", "cheese and tomato", 50); //option 1
-        Order pepperoniOrder = new Order(2, "Pepperoni", "cheese, tomato and pepperoni", 62); //option 2
-        when(c1.makeOrder()).thenReturn(margeritaOrder); //when makeOrder is called with c1, return a margerita order...
-        when(c2.makeOrder()).thenReturn(pepperoniOrder); //when makeOrder is called with c2, return a pepperoni order...
-
-        orders.add(c1.makeOrder()); //calling makeOrder with c1, and adds the pizza to the order list...
-        orders.add(c2.makeOrder()); //calling makeOrder with c2, and adds the pizza to the order list...
-
-        Mockito.doAnswer((customer) -> {
-            if (customer.getArgument(0).equals(c1)) { //if employee wants to remove order from c1, remove it from list...
-                System.out.println("removing margerita order...");
-                orders.remove(margeritaOrder);
-            }
-            if (customer.getArgument(0).equals(c2)) { //if customer wants to remove order from c2, remove it from list...
-                System.out.println("removing pepperoni order...");
-                orders.remove(pepperoniOrder);
-            }
-            return null;
-        }).when(e).deleteOrder(anyObject());
-
-        verify(c1).makeOrder(); //verify c1 has made an order
-        verify(c2).makeOrder(); //verify c2 has made an order
-
-        Assert.assertThat(orders.size(), CoreMatchers.equalTo(2)); //assert that there are 2 pizzas in the list
-        e.deleteOrder(c1); //delete the order from cusomter 1...
-        Assert.assertThat(orders.size(), CoreMatchers.equalTo(1)); //assert that there is 1 pizza in the list
     }
 }
